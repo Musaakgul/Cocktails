be48ad3540da443bce47d7f58edc1f97bbe34d8b
Musaakgul <akglmsa@gmail.com>
2023-03-07 14:59:39 +0300

    Kokteyl

---
+++ src/pages/About.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+    <p>
+    We are a 'cocktail design' and consultancy' brand whose mission is to introduce and adopt 
+    the liquor culture with our own perspective. <br></br>
+    <br></br>
+
+The CocktailDB reflects the current and future beverage trends to restaurants, 
+cafes and bars with our consultancy service, 
+based on the necessity of updating the multi-stage old styles of the food and beverage industry. 
+Creates customer and sales oriented beverage menus.
+    </p>
+    </section>
+  )
+}
+
+export default About;
+  
---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import CocktailList from '../components/CocktailList'
+import SearchForm from '../components/SearchForm'
+
+const Home = () => {
+  return (
+    <main>
+      <CocktailList/>
+      <SearchForm/>
+    </main>
+  )
+}
+ 
+export default Home;
